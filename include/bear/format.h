9d2d21072d0a9569515e29046d2eb9b70c2af38c
fixiong <fifixiong@hotmail.com>
2020-08-20 19:36:47 +0800

    update

---
+++ include/bear/format.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "bear_exception.h"
+#include <sstream>
+
+namespace bear
+{
+	void __string_format(std::ostringstream& os, const_string_ptr fmt)
+	{
+		auto f = fmt.split('%');
+		if (!f.second.empty())
+			throw bear_exception(exception_type::size_different, literal_u8("too more %!"));
+
+		os << f.first;
+	}
+
+	template<typename _Fst, typename ... _Types>
+	void __string_format(std::ostringstream &os, const_string_ptr fmt, _Fst fst, _Types&& ... _args)
+	{
+		auto f = fmt.split('%');
+		if (f.second.empty())
+			throw bear_exception(exception_type::size_different, literal_u8("too more argument!"));
+
+		os << f.first << fst;
+		__string_format(os, f.second, std::forward<_Types>(_args) ...);
+	}
+
+	template<typename ... _Types>
+	std::string format(const_string_ptr fmt, _Types&& ... _args)
+	{
+		std::ostringstream os;
+		__string_format(os, fmt, std::forward<_Types>(_args) ...);
+		return os.str();
+	}
+}
