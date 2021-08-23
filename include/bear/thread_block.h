e42b5ffc29f6bd7a7f4978cb7a4693ee9b06dfbd
fixiong <suzhiyun@yidian-inc.com>
2021-08-23 14:00:57 +0800

    update

--- include/bear/thread_block.h
+++ include/bear/thread_block.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <thread>
 #include <condition_variable>
@@ -11,7 +11,6 @@ namespace bear
 {
 	class thread_block_data
 	{
-		std::thread thread;
 		std::mutex mtx;
 		std::condition_variable inner_signal;
 		bear::functor<void> work;
@@ -22,6 +21,7 @@ namespace bear
 		bool outer_working = false;
 
 	public:
+		std::thread thread;
 
 		bool initialized()
 		{
@@ -139,6 +139,14 @@ namespace bear
 		}
 	public:
 
+		enum priority
+		{
+			priority_default,
+			priority_normal,
+			priority_high,
+			priority_low
+		};
+
 		template<typename _Res>
 		static std::pair<thread_block, _Res* > create()
 		{
@@ -167,9 +175,15 @@ namespace bear
 			return std::make_pair(std::move(block), ptr);
 		}
 
-		thread_block()
+		static void set_native_priority(std::thread::native_handle_type handle, thread_block::priority p);
+
+		thread_block(priority p = priority_default)
 		{
 			m_data = m_data.create();
+
+#if defined(THREAD_BLOCK_PRIORITY)
+			set_native_priority(m_data->thread.native_handle(), p);
+#endif
 		}
 
 		bool running()
@@ -192,4 +206,20 @@ namespace bear
 			m_data->_run(std::forward<_T>(work));
 		}
 	};
+
+	inline void run_all(array_ptr<thread_block> threads)
+	{
+	}
+
+	template<typename _Fun, typename ... _OF>
+	inline void run_all(array_ptr<thread_block> threads, _Fun&& fun, _OF&& ... of)
+	{
+		auto& p = threads.front();
+
+		p.run(std::forward<_Fun>(fun));
+
+		run_all(threads.clip(1, threads.size()), std::forward<_OF>(of) ...);
+
+		p.wait();
+	}
 }
---
+++ src/thread_block.cpp
@@ -0,0 +1,75 @@
+﻿
+#if !defined(THREAD_BLOCK_PRIORITY)
+#define THREAD_BLOCK_PRIORITY
+#endif
+
+#include <bear/thread_block.h>
+
+#if defined(_WIN32)
+#include <Windows.h>
+#else
+#include <pthread.h>
+#endif
+
+namespace bear
+{
+	void thread_block::set_native_priority(std::thread::native_handle_type handle, thread_block::priority p)
+	{
+
+#if defined(_WIN32)
+
+		if (priority_normal == p || priority_default == p) return;
+
+		DWORD dwPriorityClass = NORMAL_PRIORITY_CLASS;
+		int nPriorityNumber = 8;
+		if (priority_high == p)
+		{
+			dwPriorityClass = HIGH_PRIORITY_CLASS;
+			nPriorityNumber = THREAD_PRIORITY_HIGHEST;
+		}
+		else
+		{
+			dwPriorityClass = BELOW_NORMAL_PRIORITY_CLASS;
+			nPriorityNumber = THREAD_PRIORITY_LOWEST;
+		}
+		int result = SetPriorityClass(
+			reinterpret_cast<HANDLE>(handle),
+			dwPriorityClass);
+		if (result != 0) {
+			throw bear_exception(exception_type::multithread_error, "Setting priority class failed with ");
+		}
+		result = SetThreadPriority(
+			reinterpret_cast<HANDLE>(handle),
+			nPriorityNumber);
+		if (result != 0) {
+			throw bear_exception(exception_type::multithread_error, "Setting priority number failed with ");
+		}
+#else
+		if (priority_default == p) return;
+
+		auto low = sched_get_priority_min(SCHED_FIFO);
+		auto high = sched_get_priority_max(SCHED_FIFO);
+
+		sched_param sch_params;
+
+		if (priority_low == p)
+		{
+			sch_params.sched_priority = sched_get_priority_min(SCHED_FIFO);
+		}
+		else if (priority_high == p)
+		{
+			sch_params.sched_priority = sched_get_priority_max(SCHED_FIFO);
+		}
+		else
+		{
+			sch_params.sched_priority = (sched_get_priority_min(SCHED_FIFO) + sched_get_priority_max(SCHED_FIFO)) / 2;
+		}
+
+		if (pthread_setschedparam(th.native_handle(), SCHED_FIFO, &sch_params)) {
+			throw bear_exception(exception_type::multithread_error, "Failed to set Thread scheduling : ", std::strerror(errno));
+		}
+#endif
+
+	}
+
+}
